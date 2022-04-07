8f520468dd4bcd6901fbe27361b1c78c17001090
offsujin <offsujin@gmail.com>
2022-04-07 15:25:44 +0900

    Change admin view

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', "like_cnt", 'created_at', 'modified_at')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', "content", 'created_at', 'modified_at')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
