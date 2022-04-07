3d5d805cddee39b055e8f4706f0cdcc72ff34909
offsujin <offsujin@gmail.com>
2022-04-07 11:43:30 +0900

    Add Serializer

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import *
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['user', 'content', 'like_cnt', 'comments']
