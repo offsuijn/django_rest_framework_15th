f04ba2ddee833b7b32377d3b0ff184dc6f8ef7c5
offsujin <offsujin@gmail.com>
2022-05-08 21:38:49 +0900

    Remove last / from API

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from api import views
 from api.views import *
 
 urlpatterns = [
-    path('posts/', PostList.as_view()),
-    path('posts/<int:pk>/', PostDetail.as_view())
+    path('posts', PostList.as_view()),
+    path('posts/<int:pk>', PostDetail.as_view())
 ]
