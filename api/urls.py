049b916031b30292fa627e26f508da93b6cee5dd
offsujin <offsujin@gmail.com>
2022-05-14 23:12:31 +0900

    Url mapping using router

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from api import views
-from api.views import *
+from rest_framework import routers
+from .views import *
 
-urlpatterns = [
-    path('posts', PostList.as_view()),
-    path('posts/<int:pk>', PostDetail.as_view())
-]
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = router.urls
